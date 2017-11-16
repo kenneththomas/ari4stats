c011cb231b77e15ba93497dd8a60db874df51377
kenneththomas <kennethwoo11@gmail.com>
2017-11-16 01:35:16 -0500

    userstats needs header xd

--- userstats.sh
+++ userstats.sh
@@ -2,5 +2,8 @@ USERS=`grep \#[0-9] Daily/ari4.log|grep :| cut -d ' ' -f 4|cut -d '#'  -f1|sort 
 
 for chatter in $USERS
 do
-grep $chatter dailystats/*.csv > userstats/$chatter.csv
+#header
+echo Date,0:00,1:00,2:00,3:00,4:00,5:00,6:00,7:00,8:00,9:00,10:00,11:00,12:00,13:00,14:00,15:00,16:00,17:00,18:00,19:00,20:00,21:00,22:00,23:00,TOTAL > userstats/$chatter.csv
+
+grep $chatter dailystats/*.csv >> userstats/$chatter.csv
 done
