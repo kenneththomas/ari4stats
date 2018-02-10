87fee9fc90746bb87dd644d9cdd360b261186a01
kenneththomas <kennethwoo11@gmail.com>
2018-02-09 21:51:13 -0500

    experimental sqlite logger that we can move to instead of using text logs

---
+++ db/lumberyack.py
@@ -0,0 +1,33 @@
+import discord
+import sqlite3
+from datetime import datetime
+
+client = discord.Client()
+db = sqlite3.connect('chatlog.db')
+d = db.cursor()
+#datetime, user, channel, message
+
+@client.event
+async def on_message(message):
+    timessquare = str(datetime.now())
+
+    #we r inserting this data into the database
+    print(timessquare + ' ' + str(message.author) + ' ' + str(message.channel) + ' ' + message.content)
+
+    #make the rows
+
+    for rowboat in [(timessquare, str(message.author), str(message.channel), message.content)]:
+        d.execute('INSERT INTO log VALUES (?,?,?,?)', rowboat)
+    db.commit()
+
+
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(maricon.key)
